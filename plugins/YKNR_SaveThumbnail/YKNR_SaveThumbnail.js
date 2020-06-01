02171ca7f1d52011eeab5d466b3f3a9e5fc9d95c
Yakinori0424 <n.nakada0424@gmail.com>
2020-06-02 02:01:13 +0900

    バージョン表記更新漏れ対応

--- plugins/YKNR_SaveThumbnail/YKNR_SaveThumbnail.js
+++ plugins/YKNR_SaveThumbnail/YKNR_SaveThumbnail.js
@@ -1,5 +1,5 @@
 //============================================================================
-// YKNR_SaveThumbnail.js - ver.1.1.0
+// YKNR_SaveThumbnail.js - ver.1.1.1
 // ---------------------------------------------------------------------------
 // Copyright (c) 2019 Yakinori
 // This software is released under the MIT License.
